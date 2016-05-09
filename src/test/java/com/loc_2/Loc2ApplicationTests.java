6a6bac21badda4c4eef3fa4ace0bc0e170a2ad50
Xibuk <kubix_b@wp.pl>
2016-05-09 23:31:19 +0200

    New version of old conception :)

---
+++ src/test/java/com/loc_2/Loc2ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.loc_2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Loc2Application.class)
+@WebAppConfiguration
+public class Loc2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
